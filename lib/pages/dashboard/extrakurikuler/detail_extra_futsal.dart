df6ab257768156c731cd2d117a98c3b459a75514
ZakkyPahlevi7 <gitzakkypahlevi@gmail.com>
2023-02-14 21:44:42 +0700

    experimental melakukan page route untuk detail extra

---
+++ lib/pages/dashboard/extrakurikuler/detail_extra_futsal.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailExtraFutsal extends StatefulWidget {
+  const DetailExtraFutsal({super.key});
+
+  @override
+  State<DetailExtraFutsal> createState() => _DetailExtraFutsalState();
+}
+
+class _DetailExtraFutsalState extends State<DetailExtraFutsal> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold());
+  }
+}
---
+++ lib/pages/dashboard/extrakurikuler/detail_extra_naghdam.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailExtraNaghdam extends StatefulWidget {
+  const DetailExtraNaghdam({super.key});
+
+  @override
+  State<DetailExtraNaghdam> createState() => _DetailExtraNaghdamState();
+}
+
+class _DetailExtraNaghdamState extends State<DetailExtraNaghdam> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold());
+  }
+}
